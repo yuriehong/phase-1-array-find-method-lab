99755c81689049012fd8f0085a7ddc963d733f44
+ <+>
2022-07-21 17:11:48 -0400

    commit tree 6f8c95e9cd84379e18fb996e309c5b2963c0bd64

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function isW(element, index, array) {
+    if(element.result == "W"){
+        return element;
+    }
+
+
+  }
+function superbowlWin(objects){
+    const a =objects.find(isW);
+    if (a == undefined){
+        return a;
+    }
+    else{
+        return a.year;
+    }
+
+}
